bd03c438940146444ba7183745f3cbf84afd0e0b
SolaymanMollik <mdsolayman7814@gmail.com>
2024-11-09 11:42:03 +0600

    Thanks.

---
+++ hero.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  gravcursor: true,
+  loop: true,
+  autoplay: {
+    delay: 4000,
+  },
+});
